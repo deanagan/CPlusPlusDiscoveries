bb8b799a649ec6be54318ffa15aaf0bdd6e280c7
Dean Agan <agandfr@gmail.com>
2019-09-05 13:13:18 +1000

    Added capture rules and lambda syntax entries.

--- js/index.js
+++ js/index.js
@@ -171,5 +171,114 @@ var algosample = new Vue({
 
         ]
     }
+})
 
-});
+var lambdaSyntax = new Vue({
+    el: '.lambdasyntax',
+    data : {
+        codesamples : [
+            replaceDoubleSpaceStrings(`// Minimal lambda
+            auto fn = [] { };`),
+            replaceDoubleSpaceStrings(`auto fn = [] /* the square bracket is a "lambda introducer" */
+            { };`),
+            replaceDoubleSpaceStrings(`auto fn = [/*anything that goes here is a "capture"*/] { };`),
+            replaceDoubleSpaceStrings(`auto fn = [/*"capture"*/]
+            /* Everything in between is optional:
+             (...) mutable throwSpec -> retType */
+            { /* body */ };`),
+            replaceDoubleSpaceStrings(`// A lambda can have a parameter specified in parentheses.
+            // This is also called a lambda declarator.
+            auto fn = [](const std::string& name)
+            { std::cout << "Hello, " << name; };`),
+        ]
+    }
+})
+
+
+var captureRules = new Vue({
+    el: '.capturerules',
+    data : {
+        readchunk : 4,
+        codeexp : [
+            {
+                column1 : '[=]{};',
+                column2 : 'Outer scope is passed to the lambda by value default.'
+            },
+            {
+                column1 : '[&]{};',
+                column2 : 'Outer scope is passed to the lambda by reference default.'
+            },
+            {
+                column1 : '[&i]{};',
+                column2 : 'Capture i by reference.'
+            },
+            {
+                column1 : '[i]{};',
+                column2 : 'Capture i by value.'
+            },
+            {
+                column1 : '[&i, j]{};',
+                column2 : 'Capture i by reference and capture j by value.'
+            },
+            {
+                column1 : '[=, &i]{};',
+                column2 : 'Outer scope is passed to the lambda by value default, except i is captured by reference.'
+            },
+            {
+                column1 : '[&, i]{};',
+                column2 : 'Outer scope is passed to the lambda by reference default, except i is captured by value.'
+            },
+            {
+                column1 : '[&i, =]{};',
+                column2 : 'Error! A value default can only appear at the beginning of a lambda capture list.'
+            },
+            {
+                column1 : '[i, &]{};',
+                column2 : 'Error! A reference default can only appear at the beginning of a lambda capture list.'
+            },
+            {
+                column1 : '[&, &i]{};',
+                column2 : 'Error! Not allowed when the default capture mode is by-reference.'
+            },
+            {
+                column1 : '[&, this] {};',
+                column2 : 'Equivalent to [&].'
+            },
+            {
+                column1 : '[&, this, i]{};',
+                column2 : 'Equivalent to [&, i].'
+            },
+            {
+                column1 : '[=, *this]{};',
+                column2 : 'Invalid syntax before C++17. Valid in C++17, captures enclosing by copy.'
+            },
+            {
+                column1 : '[=, this] {};',
+                column2 : 'Invalid Syntax before C++20. Valid in C++20. Equivalent to [=].'
+            },
+            {
+                column1 : '[i, i] {};',
+                column2 : 'Error! i repeated.'
+            },
+            {
+                column1 : '[this, *this] {};',
+                column2 : 'Error! Invalid Syntax. "this" repeated.'
+            },
+        ],
+    },
+    computed : {
+        splitted() {
+            let newArr = [...this.codeexp]
+            let chunkSz = this.readchunk
+            let splitLen = Math.floor(newArr.length/chunkSz)
+            if ((newArr.length % chunkSz) > 0) {
+                splitLen += 1
+            }
+            let splittedList = []
+            for (const i of Array(splitLen).keys()) {
+                splittedList.push(newArr.slice(i*chunkSz,(i*chunkSz+chunkSz)))
+            }
+            return splittedList
+        }
+    }
+})
