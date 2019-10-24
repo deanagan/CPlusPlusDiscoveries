f59630614dff88c5bad15c0248f116283c259e36
Dean Agan <agandfr@gmail.com>
2019-10-24 15:02:25 +1100

    Adding partial work on iterator

--- js/index.js
+++ js/index.js
@@ -454,3 +454,40 @@ var lambdaGotchas = new Vue({
         ]
     }
 })
+
+
+var iteratorDescriptions = new Vue({
+    el: '.iteratorDescriptions',
+    data: {
+        descriptions : [
+            'Are concepts of an object that represents positions of elements in a container.',
+            'Not a pointer but rather, a generalization of a pointer, thus a pointer is an iterator but an iterator is not neccesarily a pointer.\
+            e.g. an iterator to a graph or tree is much more than a pointer.',
+            'Iterators share the same interface but have different types.',
+        ]
+    }
+
+})
+
+
+var iteratorFunctions = new Vue({
+    el: '.iteratorFunctions',
+    data: {
+        funcs : [
+            {
+                name : "begin/end",
+                drawing : 'images/begin-end-iter.png',
+                descriptions : [
+                "sdasdasd"
+                ]
+            },
+            {
+                name : "begin/end",
+                descriptions : [
+                "sdasdasd"
+                ]
+            },
+        ]
+    }
+
+});
