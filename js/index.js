4959a3c19fd5945b9ab47b1148e4837d063fef4a
Dean Agan <agandfr@gmail.com>
2019-10-25 11:49:00 +1100

    Adding more iterator entries

--- js/index.js
+++ js/index.js
@@ -478,15 +478,150 @@ var iteratorFunctions = new Vue({
                 name : "begin/end",
                 drawing : 'img/begin-end-iter.png',
                 descriptions : [
-                "sdasdasd"
-                ]
+                "begin represents the beginning of the elements in the container.",
+                "end represents the position after the last element, a past-the-end iterator.",
+                "cbegin/cend for constant iterator."
+                ],
+                note: "In modern C++, free function begin/end are preferred over member functions since they work on C-arrays too. i.e. begin(v) over v.begin()",
             },
             {
                 name : "rbegin/rend",
                 drawing : 'img/rbegin-rend-iter.png',
                 descriptions : [
-                "sdasdasd"
+                "rbegin represents the last element in the container.",
+                "rend represents the position before the first element in the container."
                 ]
+            },
+            {
+                name : "advance",
+                descriptions : [
+                'Advances iterator based on argument n.',
+                'Modifies its argument and returns nothing.',
+                'Does not check when it crosses end() causing undefined behavior.'
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                std::vector<int> v{ 3, 1, 4 };
+                auto vi = v.begin(); // *vi == 3
+                std::advance(vi, 2); // *vi == 4
+                `)
+            },
+            {
+                name : "next",
+                descriptions : [
+                'Advances iterator based on argument n.',
+                'Unlike advance, argument is unmodified.',
+                'Returns a copy of the argument advanced by n.',
+                'Does not check when it crosses end() causing undefined behavior.'
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                std::vector<int> v{ 3, 1, 4 };
+
+                auto it = v.begin(); // *it == 3
+                auto nx = std::next(it, 2); // *nx == 4
+                `)
+            },
+
+            {
+                name : "prev",
+                descriptions : [
+                'Decrements iterator based on argument n.',
+                'Returns a copy of the argument decremented by n.',
+                "Doesn't check if it's past begin() and may result in undefined behavior."
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                std::vector<int> v{ 3, 1, 4 };
+
+                auto it = v.end();
+                auto pv = std::prev(it, 2); // *pv == 1
+                `)
+            },
+            {
+                name : "ostream_iterator",
+                descriptions : [
+                'Single pass iterator that writes characters into  std::basic_ostream.',
+                'ostream_iterator constructs/destructs object once per character, thus could have efficiency issues.',
+                'ostream_iterator allows for delimeter when constructed.'
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                vector<int> v { 1,2,3,4,5};
+
+                copy(begin(v), end(v),
+                    ostream_iterator<int>(cout, ", ")); //1, 2, 3, 4, 5
+                `)
+            },
+            {
+                name : "iter_swap",
+                descriptions : [
+                'Swap values of the elements the given iterators are pointing to.',
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                vector<int> v { 1,2,3,4,5};
+
+                iter_swap(begin(v), prev(v.end()));
+                copy(begin(v), end(v),
+                    ostream_iterator<int>(cout, ", ")); //5, 2, 3, 4, 1
+                `)
+            },
+
+            {
+                name : "move_iterator",
+                descriptions : [
+                'Dereferencing converts the value returned by the underlying iterator into an rvalue.',
+                'If it is used as an input iterator, the effect is that the values are moved from, rather than copied from.'
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                vector<string> tmp { "One", "Two", "Three"};
+                using vstr_iterator = vector<string>::iterator;
+                vector<string> fnl(move_iterator<vstr_iterator>(begin(tmp)),
+                                      move_iterator<vstr_iterator>(end(tmp)));
+                copy(begin(tmp), end(tmp), ostream_iterator<string>(cout, ", "));
+                // "", "", ""
+                copy(begin(fnl), end(fnl), ostream_iterator<string>(cout, ", "));
+                // One, Two, Three
+                `)
+            },
+            {
+                name : "distance",
+                descriptions : [
+                'Determines distance between 2 iterators.',
+                'For random-access iterators, this function simply returns pos2-pos1.',
+                'For other iterator categories(forward iterators, etc), it increments until it hits pos2.',
+                'Has bad performance O(n) for non random access iterators.',
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                vector<string> tmp { "One", "Two", "Three"};
+                distance(begin(tmp), end(tmp)); // returns 3
+                `)
+            },
+            {
+                name : "back_inserter",
+                descriptions : [
+                'Describe me',
+                ],
+                // sample: dedentStrUsing1stLineIndent(`
+                // vector<string> tmp { "One", "Two", "Three"};
+                // distance(begin(tmp), end(tmp)); // returns 3
+                // `)
+            },
+            {
+                name : "front_inserter",
+                descriptions : [
+                'Describe me',
+                ],
+                // sample: dedentStrUsing1stLineIndent(`
+                // vector<string> tmp { "One", "Two", "Three"};
+                // distance(begin(tmp), end(tmp)); // returns 3
+                // `)
+            },
+            {
+                name : "inserter",
+                descriptions : [
+                'Describe me',
+                ],
+                // sample: dedentStrUsing1stLineIndent(`
+                // vector<string> tmp { "One", "Two", "Three"};
+                // distance(begin(tmp), end(tmp)); // returns 3
+                // `)
             },
         ]
     }
