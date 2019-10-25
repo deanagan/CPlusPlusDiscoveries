d68b1a979b0d0318b71cdc8c8706b9157c8925b7
Dean Agan <agandfr@gmail.com>
2019-10-25 15:13:44 +1100

    Add more entries for iterator functions

--- js/index.js
+++ js/index.js
@@ -497,7 +497,7 @@ var iteratorFunctions = new Vue({
                 descriptions : [
                 'Advances iterator based on argument n.',
                 'Modifies its argument and returns nothing.',
-                'Does not check when it crosses end() causing undefined behavior.'
+                'Does not check when it crosses end() which may cause undefined behavior.'
                 ],
                 sample: dedentStrUsing1stLineIndent(`
                 std::vector<int> v{ 3, 1, 4 };
@@ -511,7 +511,7 @@ var iteratorFunctions = new Vue({
                 'Advances iterator based on argument n.',
                 'Unlike advance, argument is unmodified.',
                 'Returns a copy of the argument advanced by n.',
-                'Does not check when it crosses end() causing undefined behavior.'
+                'Does not check when it crosses end() which may cause undefined behavior.'
                 ],
                 sample: dedentStrUsing1stLineIndent(`
                 std::vector<int> v{ 3, 1, 4 };
@@ -562,24 +562,6 @@ var iteratorFunctions = new Vue({
                     ostream_iterator<int>(cout, ", ")); //5, 2, 3, 4, 1
                 `)
             },
-
-            {
-                name : "move_iterator",
-                descriptions : [
-                'Dereferencing converts the value returned by the underlying iterator into an rvalue.',
-                'If it is used as an input iterator, the effect is that the values are moved from, rather than copied from.'
-                ],
-                sample: dedentStrUsing1stLineIndent(`
-                vector<string> tmp { "One", "Two", "Three"};
-                using vstr_iterator = vector<string>::iterator;
-                vector<string> fnl(move_iterator<vstr_iterator>(begin(tmp)),
-                                      move_iterator<vstr_iterator>(end(tmp)));
-                copy(begin(tmp), end(tmp), ostream_iterator<string>(cout, ", "));
-                // "", "", ""
-                copy(begin(fnl), end(fnl), ostream_iterator<string>(cout, ", "));
-                // One, Two, Three
-                `)
-            },
             {
                 name : "distance",
                 descriptions : [
@@ -594,34 +576,81 @@ var iteratorFunctions = new Vue({
                 `)
             },
             {
+                name : "move_iterator",
+                descriptions : [
+                'Dereferencing converts the value returned by the underlying iterator into an rvalue.',
+                'If it is used as an input iterator, the effect is that the values are moved from, rather than copied from.'
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                vector<string> tmp { "One", "Two", "Three"};
+                using vstr_iterator = vector<string>::iterator;
+                vector<string> fnl(move_iterator<vstr_iterator>(begin(tmp)),
+                                      move_iterator<vstr_iterator>(end(tmp)));
+                copy(begin(tmp), end(tmp), ostream_iterator<string>(cout, ", "));
+                // Output: "", "", ""
+                copy(begin(fnl), end(fnl), ostream_iterator<string>(cout, ", "));
+                // Output: One, Two, Three
+                `)
+            },
+            {
+                name : "make_move_iterator",
+                descriptions : [
+                'A convenience function template that constructs a move_iterator.',
+                ],
+                sample: dedentStrUsing1stLineIndent(`
+                vector<string> tmp { "One", "Two", "Three"};
+                vector<string> fnl(make_move_iterator(begin(tmp)),
+                                   make_move_iterator(end(tmp)));
+
+                copy(begin(tmp), end(tmp), ostream_iterator<string>(cout, ", "));
+                // Output: "", "", ""
+                copy(begin(fnl), end(fnl), ostream_iterator<string>(cout, ", "));
+                // Output: One, Two, Three
+                `)
+            },
+            {
                 name : "back_inserter",
                 descriptions : [
-                'Describe me',
+                    'Allow algorithms to work with containers without knowing the size beforehand',
+                    'Constructs a back_insert_iterator for the container with the type deduced from the type of the argument.',
+                    'Works on containers that support a push_back operation.'
                 ],
-                // sample: dedentStrUsing1stLineIndent(`
-                // vector<string> tmp { "One", "Two", "Three"};
-                // distance(begin(tmp), end(tmp)); // returns 3
-                // `)
+                sample: dedentStrUsing1stLineIndent(`
+                vector<int> src { 1, 2, 3, 4, 5};
+                vector<int> dest;
+                transform(begin(src), end(src), back_inserter(dest),
+                    [](int v) { return v*2; });
+                // Output: dest = 2, 4, 6, 8, 10
+                `)
             },
             {
                 name : "front_inserter",
                 descriptions : [
-                'Describe me',
+                    'Allow algorithms to work with containers without knowing the size beforehand',
+                    'Constructs a front_insert_iterator for the container with the type deduced from the type of the argument.',
+                    'Works on containers that support a push_front operation(mostly deques).'
                 ],
-                // sample: dedentStrUsing1stLineIndent(`
-                // vector<string> tmp { "One", "Two", "Three"};
-                // distance(begin(tmp), end(tmp)); // returns 3
-                // `)
+                sample: dedentStrUsing1stLineIndent(`
+                vector<int> src { 1, 2, 3, 4, 5};
+                deque<int> dest;
+                copy(begin(src), end(src), front_inserter(dest));
+                // Output: dest = 5, 4, 3, 2, 1
+                `)
             },
             {
                 name : "inserter",
                 descriptions : [
-                'Describe me',
+                    'Allow algorithms to work with containers without knowing the size beforehand',
+                    'Constructs an insert_iterator for the container and its iterator with the type deduced from the type of the argument.',
+                    'Works on containers that support insert operation.'
                 ],
-                // sample: dedentStrUsing1stLineIndent(`
-                // vector<string> tmp { "One", "Two", "Three"};
-                // distance(begin(tmp), end(tmp)); // returns 3
-                // `)
+                sample: dedentStrUsing1stLineIndent(`
+                vector<int> d { 7, 4, 3, 2, 5, 1, 6 };
+                set<int> s;
+                // Because sets are ordered, the output is arranged in ascending sorted order.
+                copy(begin(d), end(d), inserter(s, begin(s)));
+                // Output: dest = 1, 2, 3, 4, 5, 6, 7
+                `)
             },
         ]
     }
